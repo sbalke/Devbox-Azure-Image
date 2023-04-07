3d5b1f727ba376c2b306b4a703ff5fbaf8d7eacc
Steve Balke <sbalke@gmail.com>
2023-04-07 19:15:16 -0400

    trying to add missing package

--- Image Builder/installwinget.ps1
+++ Image Builder/installwinget.ps1
@@ -3,13 +3,17 @@ Start-Transcript -Path "C:\buildartifacts\installwinget.log"
 $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
 
 
-if(!$hasPackageManager)
-{
+if (!$hasPackageManager) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    #Install NuGet Package Provider. For future use
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    #Install Package Provider Source. For future use
+    Register-PackageSource -provider NuGet -name nugetRepository -location https://www.nuget.org/api/v2
+
     # download
     Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx' -UseBasicParsing
-    
-    Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-
+  
     $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
 
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -18,6 +22,9 @@ if(!$hasPackageManager)
 
     Invoke-WebRequest -Uri $($latestRelease.browser_download_url) -OutFile 'Microsoft.DesktopAppInstaller.msixbundle' -UseBasicParsing
 
+    Install-Package Microsoft.UI.Xaml -RequiredVersion 2.7.3 
+
+    Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
     Add-AppxPackage -Path 'Microsoft.DesktopAppInstaller.msixbundle'
 
     # delete file
