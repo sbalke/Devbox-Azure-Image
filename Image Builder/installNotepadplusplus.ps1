b5712ff8e17e72bf5baa8467bd3f0d171f9e6419
Steve Balke <sbalke@gmail.com>
2023-04-11 21:09:40 -0400

    transcripts

--- Image Builder/installNotepadplusplus.ps1
+++ Image Builder/installNotepadplusplus.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\buildartifacts\installdocker.log"
+Start-Transcript -Path "C:\buildartifacts\installnotepad++.log"
 
 winget install Notepad++.Notepad++ -h --disable-interactivity --accept-package-agreements --accept-source-agreements
 
--- Image Builder/installSSMS.ps1
+++ Image Builder/installSSMS.ps1
@@ -1,2 +1,5 @@
+Start-Transcript -Path "C:\buildartifacts\installssms.log"
+
 winget install Microsoft.SQLServerManagementStudio -h --disable-interactivity --accept-package-agreements --accept-source-agreements
-echo DockerDesktop-Installed >> c:\\buildArtifacts\\customize.txt
+
+Stop-Transcript
