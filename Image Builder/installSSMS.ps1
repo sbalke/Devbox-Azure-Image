6c5a00d0c612463da61a88a7e4128913879b3b69
Steve Balke <steve.balke@etchasoft.com>
2023-05-24 11:02:51 -0400

    changes

--- Image Builder/installSSMS.ps1
+++ Image Builder/installSSMS.ps1
@@ -1,9 +1,9 @@
 Start-Transcript -Path "C:\buildartifacts\installssms.log"
 
-if($useChoco) {
+#if($useChoco) {
     choco install -y sql-server-management-studio
-} else {
-    winget install Microsoft.SQLServerManagementStudio -h --disable-interactivity --accept-package-agreements --accept-source-agreements
-}
+#} else {
+#    winget install Microsoft.SQLServerManagementStudio -h --disable-interactivity --accept-package-agreements --accept-source-agreements
+#}
 
 Stop-Transcript
