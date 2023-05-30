047355fa0126ad7d21030bb8986826fb41568361
Steve Balke <sbalke@gmail.com>
2023-05-30 10:32:27 -0400

    Another attempt

--- Manual Image/Setup-Neach.ps1
+++ Manual Image/Setup-Neach.ps1
@@ -1,4 +1,4 @@
-& ..\Image Builder\Install-Chocolatey.ps1
-& ..\Image Builder\Install-Dotnet.ps1
-& ..\Image Builder\Install-SSMS.ps1
-& ..\Image Builder\Set-Theme.ps1
+& "..\Image Builder\Install-Chocolatey.ps1"
+& "..\Image Builder\Install-Dotnet.ps1"
+& "..\Image Builder\Install-SSMS.ps1"
+& "..\Image Builder\Set-Theme.ps1"
