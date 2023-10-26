d0302037f35fcc624777f38a5a6c09c5f80645d6
Steve Balke <steve.balke@etchasoft.com>
2023-10-26 09:49:03 -0400

    Adding GMC

---
+++ scripts/Install-GMC.ps1
@@ -0,0 +1,6 @@
+& C:\BuildArtifacts\Install-SSMS.ps1
+& C:\BuildArtifacts\Install-Notepadplusplus.ps1
+& C:\BuildArtifacts\Install-AzureStorageExplorer.ps1
+& C:\BuildArtifacts\Install-Filezilla.ps1
+& C:\BuildArtifacts\Fix-Sysprep.ps1
+& C:\BuildArtifacts\Remove-AppxPackages.ps1
